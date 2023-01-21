ccaaf4af011a1cab99b0925c116684d87cf0c2b2
Angela Carvalho <angelalopo@outlook.com>
2023-01-21 19:05:15 -0300

    importa carregaTarefa

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { handleNovoItem } from "./components/criaTarefa.js"
+import { handleNovoItem } from "./components/criaTarefa.js";
+import { carregaTarefa } from "./components/carregaTarefa.js";
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
