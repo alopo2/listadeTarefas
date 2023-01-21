13931b778db5e21906551366c72ff7e9cd56f71b
Angela Carvalho <angelalopo@outlook.com>
2023-01-21 19:05:39 -0300

    inicializa carregaTarefa

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ import { carregaTarefa } from "./components/carregaTarefa.js";
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', handleNovoItem)
+
+carregaTarefa()
