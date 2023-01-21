8bd1ebcae28d80f384a583d444b7f1bcce5caa6e
Angela Carvalho <angelalopo@outlook.com>
2023-01-21 18:32:08 -0300

    importa funcao handleNovoItem

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { handleNovoItem } from "./components/criaTarefa.js"
+
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', handleNovoItem)
