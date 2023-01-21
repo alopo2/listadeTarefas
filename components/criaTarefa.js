1b623d18984f96719b6081387fae5e1b78ccd9df
Angela Carvalho <angelalopo@outlook.com>
2023-01-21 18:26:02 -0300

    refatora responsabilidades

---
+++ components/criaTarefa.js
@@ -0,0 +1,47 @@
+import BotaoConclui from "./components/concluiTarefa";
+import BotaoDeleta from "./components/deletaTarefa";
+
+let tarefas = []
+
+const handleNovoItem = (evento) => {
+    evento.preventDefault()
+
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value);
+
+    const dataFormatada = data.format('DD/MM/YYYY');
+
+    const dados = {
+        valor,
+        dataFormatada
+    }
+
+    const criaTarefa = criarTarefa(dados);
+
+    tarefas.push(dados);
+
+    lista.appendChild(criaTarefa);
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+    input.value = " "
+}
+
+const criarTarefa = ({ valor, dataFormatada }) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`;
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa;
+
+}
